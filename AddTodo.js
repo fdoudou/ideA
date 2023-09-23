66d6aa9260a7982899252ea8954d7fead89cdc02
fdoudou <softgeekvert@gmail.com>
2023-09-23 03:02:35 +0000

    First commit

---
+++ AddTodo.js
@@ -0,0 +1,22 @@
+import { useContext,useReducer } from "react";
+import { MyContext } from "./Contextor";
+import { useState } from "react";
+
+function AddTodo(props) {
+    const {addTask,keyword,searchTask} = useContext(MyContext);
+    // const [taskTxt,setTaskTxt] = useState("");
+
+    // const addText = (event) => {
+    //     setTaskTxt(event.target.value);
+    // }
+
+    return (
+        <form>
+            {/* <textarea ></textarea><br/> */}
+            <button type="button" onClick={addTask()}>Add new Task?</button>
+            <input type="text" value={keyword} onChange={searchTask} placeholder="Type to search a task"></input>
+        </form>
+    )
+}
+
+export default AddTodo;
---
+++ Todo.js
@@ -0,0 +1,91 @@
+import { useState,useEffect } from "react";
+import { MyContext } from "./Contextor.js";
+import TodoList from "./TodoList.js";
+
+function Todo(props) {
+    const tlist = [
+        {
+            id:1,
+            title:'Todo 1',
+            content: "This is Todo 1",
+            complete:false,
+            status: 'not complete'
+        },
+        {
+            id:2,
+            title:'Todo 2',
+            content: "This is Todo 2",
+            complete:false,
+            status: 'not complete'
+        }
+    ];
+
+    // const saveTask = (tasklist) => {
+    //     localStorage.setItem('Todo',tasklist);
+    // }
+
+    //let cstate = localStorage.getItem('Todo') == null ? tlist : JSON.parse(localStorage.getItem('Todo'));
+
+    const [todo,todoState] = useState([]);
+    const [keyword,setKeyword] = useState("");
+    const [txt,setTxt] = useState("");
+
+    // useEffect(() => {
+    //     localStorage.setItem('Todo',JSON.stringify([...todo]))
+    // },[todo]);
+
+    const addTask = (tasktext) => {
+        const newt = todo.length + 1;
+        let ntodo = {
+            id:newt,
+            title:'Todo ' + newt,
+            content:"This is Todo " + newt,
+            complete:false,
+            status: 'not complete'
+        };
+        //todo.push(ntodo);
+        let currentTask = [...todo,ntodo];
+        todoState(currentTask);
+    }
+
+    const updateTask = (i,event) => {
+        const tsk = [...todo];
+        tsk[i].content = event.target.value;
+        let newtask = {...tsk[i]};
+        todoState([...tsk,{...newtask}]);
+        setTxt(event.target.value);
+    }
+
+    const removeTask = (c) => {
+        todo.splice(c,1);
+        todoState([...todo]);
+    }
+
+    const searchTask = (event) => {
+        setKeyword(event.target.value);
+        let tdlist = [...todo];
+        
+        let ntdlist = tdlist.filter((td) => { 
+            for (const c of JSON.stringify(td.content)) {
+                if (c.includes(event.target.value)) {
+                    return {...td};
+                }
+            }
+        });
+    }
+
+    const setComplete = (td,c) => {
+        const nlist = [...todo];
+        const tstatus = td.status === 'completed' ? 'not complete':'completed';
+        nlist[c] = {...td,complete:!td.complete,status:tstatus};
+        todoState([...nlist]);
+    }
+
+    return (
+        <MyContext.Provider value={{todo,todoState,setComplete,addTask,removeTask,keyword,setKeyword,searchTask,updateTask,txt}}>
+            <TodoList></TodoList>
+        </MyContext.Provider>
+    );
+}
+
+export default Todo;
